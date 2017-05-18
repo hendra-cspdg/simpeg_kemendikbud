81200f1d089d589a05c6357ff3fac2b816a3a8fa
yanarazor <yanarazor@gmail.com>
2017-05-18 10:06:40 +0700

    themes header and footer

--- application/modules/pegawai/assets/js/pegawai.js
+++
@@ -1,7 +0,0 @@
-
-                    $('#Tgl_Lahir').datepicker({dateFormat: 'yy-mm-dd'});
-                    $('#Tgl_SK_CPNS').datepicker({dateFormat: 'yy-mm-dd'});
-                    $('#TMT_CPNS').datepicker({dateFormat: 'yy-mm-dd'});
-                    $('#TMT_PNS').datepicker({dateFormat: 'yy-mm-dd'});
-                    $('#TMT_Golongan').datepicker({dateFormat: 'yy-mm-dd'});
-                    $('#TMT_Jabatan').datepicker({dateFormat: 'yy-mm-dd'});
