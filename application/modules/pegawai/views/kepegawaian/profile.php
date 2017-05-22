b61aadbf94bbbd56f0bd1abff5b85de0fc088d01
koharudin <koharudin.mail07@gmail.com>
2017-05-22 15:58:16 +0700

    perbaikan modular layout tab

--- application/modules/pegawai/views/kepegawaian/profile.php
+++ application/modules/pegawai/views/kepegawaian/profile.php
@@ -1,8 +1,11 @@
+<?php 
+    $tab_pane_pendidikan_id = uniqid("tab_pane_pendidikan");
+    $tab_pane_personal_id = uniqid("tab_pane_personal");
+?>
 <script src="<?php echo base_url(); ?>themes/admin/js/sweetalert.js"></script>
 <link rel="stylesheet" href="<?php echo base_url(); ?>themes/admin/css/sweetalert.css">
 <?php
-$this->load->library('convert');
-$convert = new convert();
+
 if (validation_errors()) :
 ?>
 <div class='alert alert-block alert-error fade in'>
@@ -78,10 +81,10 @@ $PNS_ID = isset($pegawai->PNS_ID) ? $pegawai->PNS_ID : '';
                                 <div class="tabbable-line tabbable-custom-profile">
                                     <ul id="tab-insides-here" class="nav nav-tabs">
                                         <li class="active">
-                                            <a href="#personal" data-toggle="tab" aria-expanded="true"> Data Personal </a>
+                                            <a href="#<?php echo $tab_pane_personal_id; ?>" data-toggle="tab" aria-expanded="true"> Data Personal </a>
                                         </li>
                                         <li class="">
-                                            <a href="#pendidikan" data-toggle="tab" aria-expanded="false"> Pendidikan </a>
+                                            <a href="#<?php echo $tab_pane_pendidikan_id; ?>" data-toggle="tab" aria-expanded="false"> Pendidikan </a>
                                         </li>
                                         <li class="">
                                             <a href="#pengalaman" data-toggle="tab" aria-expanded="false"> Pengalaman </a>
@@ -118,221 +121,10 @@ $PNS_ID = isset($pegawai->PNS_ID) ? $pegawai->PNS_ID : '';
                                         </li>
                                     </ul>
                                     <div class="tab-content">
-                                        <div class="tab-pane active" id="personal">
-                                            <form role="form" action="#">
-                                                
-                                                 
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Status Kepegawaian
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b><?php echo isset($pegawai->Status_CPNS_PNS) ? $pegawai->Status_CPNS_PNS == "P" ? "PNS" : "" : ''; ?>   (<?php echo $convert->fmtDate($pegawai->TMT_PNS,"dd month yyyy"); ?>)</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Golongan
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>III/b - Penata Muda Tingkat I</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Tanggal Masuk / Aktif
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>
-                                                                12 Jan 2010 / 01 Jan 2011                                                            </b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Tanggal Pensiun
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>
-                                                                03 Jan 2043                                                            </b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            No. Handphone
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>082260195526</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Jenis Kelamin
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>Pria</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Agama
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>Islam</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Status Pernikahan
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>Kawin</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Tempat Lahir
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b></b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Jabatan
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>Programmer Subbagian Kepegawaian dan Umum</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Kemampuan Bahasa
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b> Sunda</b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Kontak
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>
-                                                                  yana004@kemdikbud.go.id<br>yana.mardiyana@kemdikbud.go.id<br>yanarazor@gmail.com<br>-<br><br>                                                            </b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                                                            Email
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                                                            <b>
-                                                                yanarazor@gmail.com                                                            </b>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                
-                                            </form>
-                                        </div>
-                                        <!--tab-pane-->
-                                        <div class="tab-pane" id="pendidikan">
-                                            <div class="form-group">
-                                                <div class="row">
-                                                	<div class="col-lg-12 col-md-12 col-sm-21 col-xs-12">
-                                                    <?php if ($this->auth->has_permission('Pegawai.Kepegawaian.Addpendidikan')) : ?>
-													<a href="<?php echo base_url(); ?>admin/kepegawaian/pegawai/addpendidikan/<?php echo $PNS_ID ?>" class="show-modal" tooltip="Tambah Riwayat Pendidikan">
-														<button type="button" class="btn btn-default btn-warning margin pull-right "><i class="fa fa-plus"></i> Tambah</button>
-													</a>
-													<?php endif; ?>
-													<table class="table">
-													<thead>
-														<tr>
-															<th>No</th>
-															<th>Pendidikan</th>
-															<th>Tgl. Lulus</th>
-															<th>Tahun Lulus</th>
-															<th>Nomor Ijazah</th>
-															<th>Nama Sekolah</th>
-															<th>Pendidikan Pertama</th>
-															<th>#</th>
-														</tr>
-													</thead>
-													<tfoot>
-														<tr>
-															 
-														</tr>
-													</tfoot>
-													<tbody>
-														<?php
-														$NO = 1;
-														$has_records	= isset($records) && is_array($records) && count($records);
-														if ($has_records) :
-															foreach ($records as $record) :
-														?>
-														<tr>
-															<td><?php e($NO); ?>.</td>
-															<td><?php e($record->NAMA_PENDIDIKAN); ?></td>
-															<td><?php e($record->Tanggal_Lulus) ?></td>
-															<td><?php e($record->Tahun_Lulus) ?></td>
-															<td><?php e($record->Nomor_Ijasah) ?></td>
-															<td><?php e($record->Nama_Sekolah) ?></td>
-															<td><?php e($record->Pendidikan_Pertama) ?></td>
-															<td>
-															<a href="<?php echo base_url(); ?>admin/kepegawaian/pegawai/addpendidikan/<?php echo $record->PNS_ID; ?>/<?php echo $record->ID; ?>"  data-toggle='tooltip' title='Edit data' class="show-modal"><span class='fa-stack'>
-															 <i class='fa fa-square fa-stack-2x'></i>
-															 <i class='fa fa-pencil fa-stack-1x fa-inverse'></i>
-															 </span>
-															 </a>
-															 <a href="#" url="<?php echo base_url() ?>admin/kepegawaian/pegawai/deletependidikan" kode="<?php echo $record->ID; ?>" class='delete' data-toggle='tooltip' title='Hapus Data' >
-															 <span class='fa-stack'>
-															 <i class='fa fa-square fa-stack-2x'></i>
-															 <i class='fa fa-trash-o fa-stack-1x fa-inverse'></i>
-															 </span>
-															 </a>
-															 </td>
-														</tr>
-														<?php
-															endforeach;
-														else:
-														?>
-														<tr>
-															<td colspan="7">Data tidak ada</td>
-														</tr>
-														<?php endif; ?>
-													</tbody>
-												</table>  
-												</div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <!--tab-pane-->
-                                        <!-- CHANGE PASSWORD TAB -->
+                                        <?php 
+                                            $this->load->view('kepegawaian/tab_pane_personal',array('PNS_ID'=>$PNS_ID,'TAB_ID'=>$tab_pane_personal_id));                                         
+                                            $this->load->view('kepegawaian/tab_pane_riwayat_pendidikan',array('PNS_ID'=>$PNS_ID,'TAB_ID'=>$tab_pane_pendidikan_id));
+                                        ?>
                                         <div class="tab-pane" id="pengalaman">
                                             <div class="row">
                                                 <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
@@ -344,7 +136,6 @@ $PNS_ID = isset($pegawai->PNS_ID) ? $pegawai->PNS_ID : '';
                                                 </div>
                                             </div>
                                         </div>
-                                        <!-- END CHANGE PASSWORD TAB -->
                                         <!-- PRIVACY SETTINGS TAB -->
                                         <div class="tab-pane" id="kegiatan">
                                             <div class="row">
@@ -364,45 +155,7 @@ $PNS_ID = isset($pegawai->PNS_ID) ? $pegawai->PNS_ID : '';
                         </div>
     </div>
 </div>
-<script type="text/javascript">
-$(".delete").click(function(){
-	var kode =$(this).attr("kode");
-	var url =$(this).attr("url");
-	swal({
-		title: "Anda Yakin?",
-		text: "Hapus data riwayat pendidikan!",
-		type: "warning",
-		showCancelButton: true,
-		confirmButtonClass: 'btn-danger',
-		confirmButtonText: 'Ya, Delete!',
-		cancelButtonText: "Tidak, Batalkan!",
-		closeOnConfirm: false,
-		closeOnCancel: false
-	},
-	function (isConfirm) {
-		if (isConfirm) {
-			var post_data = "kode="+kode;
-			$.ajax({
-					url: url,
-					type:"POST",
-					data: post_data,
-					dataType: "html",
-					timeout:180000,
-					success: function (result) {
-						 swal("Deleted!", result, "success");
-						 location.reload(true);
-				},
-				error : function(error) {
-					alert(error);
-				} 
-			});        
-			
-		} else {
-			swal("Batal", "", "error");
-		}
-	});
-});
-</script>
+
  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/js/jquery-scrolling-tabs/jquery.scrolling-tabs.css">
  <script src="<?php echo base_url(); ?>assets/js/jquery-scrolling-tabs/jquery.scrolling-tabs.js"></script>
 
---
+++ application/modules/pegawai/views/kepegawaian/tab_pane_personal.php
@@ -0,0 +1,142 @@
+<div class="tab-pane active" id="<?php echo $TAB_ID;?>">
+    <form role="form" action="#">
+        
+            
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Status Kepegawaian
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b><?php echo isset($pegawai->Status_CPNS_PNS) ? $pegawai->Status_CPNS_PNS == "P" ? "PNS" : "" : ''; ?>   (<?php echo $this->convert->fmtDate($pegawai->TMT_PNS,"dd month yyyy"); ?>)</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Golongan
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>III/b - Penata Muda Tingkat I</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Tanggal Masuk / Aktif
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>
+                        12 Jan 2010 / 01 Jan 2011                                                            </b>
+                </div>
+            </div>
+        </div>
+        
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Tanggal Pensiun
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>
+                        03 Jan 2043                                                            </b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    No. Handphone
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>082260195526</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Jenis Kelamin
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>Pria</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Agama
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>Islam</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Status Pernikahan
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>Kawin</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Tempat Lahir
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b></b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Jabatan
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>Programmer Subbagian Kepegawaian dan Umum</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Kemampuan Bahasa
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b> Sunda</b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Kontak
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>
+                            yana004@kemdikbud.go.id<br>yana.mardiyana@kemdikbud.go.id<br>yanarazor@gmail.com<br>-<br><br>                                                            </b>
+                </div>
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    Email
+                </div>
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                    <b>
+                        yanarazor@gmail.com                                                            </b>
+                </div>
+            </div>
+        </div>
+        
+    </form>
+</div>
