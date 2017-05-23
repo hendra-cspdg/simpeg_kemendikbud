044328bc64f2bb6da1e7311a127f2babbe14a13f
yanarazor <yanarazor@gmail.com>
2017-05-23 15:50:17 +0700

    update peta jabatan

--- application/modules/petajabatan/views/reports/content.php
+++ application/modules/petajabatan/views/reports/content.php
@@ -1,11 +1,11 @@
 <center>
 	<div class="col-sm-12">
-		<div class="box-small col-sm-12"  style="border: 1px solid green;min-height:50px; padding:10px;margin-bottom:20px;background:#3c8dbc;color:white;"><?php echo isset($datadetil->NAMA_ESELON_II) ? $datadetil->NAMA_ESELON_II : ""; ?></div>
+		<div class="box-small col-sm-12"  style="border: 1px solid black;min-height:50px; padding:10px;margin-bottom:20px;background:#3c8dbc;color:white;"><?php echo isset($datadetil->NAMA_ESELON_II) ? $datadetil->NAMA_ESELON_II : ""; ?></div>
 	</div>
 	<?php if (isset($eselon3) && is_array($eselon3) && count($eselon3)):?>
 		<?php foreach($eselon3 as $record):?>
 			<div class="box-small  col-sm-3">
-			   <div class="box-small  col-sm-12" style="border: 1px solid green;background:green;min-height:50px;color:white;">
+			   <div class="box-small  col-sm-12" style="border: 1px solid black;background:green;min-height:50px;color:white;">
 				   <?php echo $record->NAMA_ESELON_III; ?>
 			   </div>
 			   
@@ -13,7 +13,7 @@
 					   $ideselon3 = isset($record->ID) ? substr($record->ID,0,8) : "";
 					   for($i=0;$i < count($aeselon4[$ideselon3]);$i++){
 					   ?>
-					   	<div class="box-small  col-sm-12" style="margin-top:20px;border: 1px solid yellow;background:yellow;min-height:50px;">
+					   	<div class="box-small  col-sm-12" style="margin-top:20px;border: 1px solid black;background:yellow;min-height:50px;">
 					   	<?php
 					   		$ideselon4 = $aeselon4[$ideselon3."-ID"][$i];
 						   	echo $aeselon4[$ideselon3][$i];
