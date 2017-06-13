1a7007e4bc88c6f54375158cf51a7b4cc92bb209
koharudin <koharudin.mail07@gmail.com>
2017-06-13 16:06:51 +0700

    add method api

--- application/modules/api/controllers/Pegawai.php
+++ application/modules/api/controllers/Pegawai.php
@@ -8,7 +8,40 @@ class Pegawai extends  LIPIAPI_REST_Controller {
     ];
 	
 	public function list_get(){
-        $data_pegawai = $this->db->from("hris.pegawai")->get()->result();
+        
+        
+        $output = array(
+            'success'=>false,
+            'msg'=>'Unknown error'
+        );
+        $unor_id = $this->get('unor_id');
+        if ($unor_id === NULL)
+        {
+            $output['msg'] = 'Parameter unor_id di butuhkan';
+            $this->response($output, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+        $data_pegawai = $this->db->from("hris.pegawai")->
+        where("UNOR_ID",$unor_id)->
+        get()->result();
+         $output = array(
+            'success' => true,
+            'data'=>$data_pegawai
+        );
+        $this->response($output, REST_Controller::HTTP_OK); // OK (200) being the HTTP response code
+    }
+    public function detail_get(){
+        $pegawai_nip = $this->input->post('nip');
+        $output = array(
+            'success'=>false,
+            'msg'=>'Unknown error'
+        );
+        $pegawai_nip = $this->get('pegawai_nip');
+        if ($pegawai_nip === NULL)
+        {
+            $output['msg'] = 'Parameter pegawai_nip di butuhkan';
+            $this->response($output, REST_Controller::HTTP_BAD_REQUEST); // BAD_REQUEST (400) being the HTTP response code
+        }
+        $data_pegawai = $this->db->from("hris.pegawai")->where('NIP_BARU',$pegawai_nip)->get()->first_row();
         $output = array(
             'success' => true,
             'data'=>$data_pegawai
