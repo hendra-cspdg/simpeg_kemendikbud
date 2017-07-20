ddafb6afe4da6681df94d078adb0896547856539
yanarazor <yanarazor@gmail.com>
2017-07-20 14:03:20 +0700

    delete meun

--- themes/admin/partials/_header.php
+++ themes/admin/partials/_header.php
@@ -221,8 +221,6 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="<?php echo base_url();?>admin/kepegawaian/pegawai"><i class="fa fa-circle-o"></i>Riwayat Pekerjaan Pegawai</a></li>
-            <li><a href="<?php echo base_url();?>admin/kepegawaian/pegawai"><i class="fa fa-circle-o"></i>Daftar Pegawai</a></li>
             <li><a href="<?php echo base_url();?>pegawai/duk"><i class="fa fa-circle-o"></i>Daftar Urut Kepangkatan</a></li>
             <li><a href="<?php echo base_url();?>admin/reports/pegawai/kelompokjabatan"><i class="fa fa-circle-o"></i>Daftar kelompok Jabatan</a></li>
             <li><a href="<?php echo base_url();?>admin/reports/petajabatan"><i class="fa fa-list"></i>Daftar daftar kuota jabatan</a></li>
